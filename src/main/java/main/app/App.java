18d00c1070beb6dd16d9facfc0cb550df47e62e1
Mathew Stacey <mathew.stacey@paconsulting.com>
2017-02-01 10:51:39 +0000

    shitty comments

--- src/main/java/main/app/App.java
+++ src/main/java/main/app/App.java
@@ -11,6 +11,27 @@ import java.util.Set;
  * FixLionel's Problem yo
  */
 public class App {
+
+    /**
+     * <h1>Entry point for App</h1>
+     *
+     * <p>Files must fit the requirements:</p>
+     *
+     *  <ul>
+     *      <li>Table has two columns</li>
+     *      <li>Two columns must be separated by a space</li>
+     *      <li>No trailing spaces</li>
+     *  </ul>
+     *
+     *  <p>
+     *      Preferred format requirements:
+     *  </p>
+     *  <ul>
+     *      <li>Column 1 is a comma separated combined key (tuple)</li>
+     *  </ul>
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         FixProblem problem = new FixProblem("/table1.txt", "/table2.txt");
         if (!problem.hasError()) {
@@ -20,14 +41,28 @@ public class App {
         }
     }
 
-
+    /**
+     * Class to handle Fixing the problem
+     */
     private static class FixProblem {
+        /**
+         * Variables to handle errors and print messages
+         */
         String message;
         boolean error = false;
 
+        /**
+         * Set of Unique Keys from each file
+         * and Map of each key with volume totals
+         */
         Set<String> uniqueKeys = new HashSet<String>();
         Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
+        /**
+         * Constructor for merging two files in a groupBy fashion
+         * @param file1 First table to merge
+         * @param file2 Second table to merge
+         */
         private FixProblem(String file1, String file2) {
             InputStream tableOne = FixProblem.class.getResourceAsStream(file1);
             InputStream tableTwo = FixProblem.class.getResourceAsStream(file2);
@@ -39,6 +74,11 @@ public class App {
             this.readFiles(tableOne, tableTwo);
         }
 
+        /**
+         * Read the two input files
+         * @param tableOne First table to read
+         * @param tableTwo Second table to read
+         */
         private void readFiles(InputStream tableOne, InputStream tableTwo) {
             try {
                 BufferedReader tableOneReader = new BufferedReader(new InputStreamReader(tableOne, "UTF-8"));
@@ -51,6 +91,11 @@ public class App {
             }
         }
 
+        /**
+         * Process file contents and place into a Set of unique keys
+         * and a map containing the sum of volumes for each key
+         * @param tableReader Reader for table
+         */
         private void processFile(BufferedReader tableReader) {
             try {
                 String strLine;
@@ -75,6 +120,10 @@ public class App {
             }
         }
 
+        /**
+         * Write results of process into new file
+         * @param outputFile Location of output file
+         */
         private void writeTo(String outputFile) {
             try {
                 PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
@@ -88,22 +137,46 @@ public class App {
             }
         }
 
+        /**
+         * Return result Map.
+         * @return key, volume Map of result.
+         *
+         * Not in use.
+         */
         Map<String, Integer> getResults() {
             return this.resultMap;
         }
 
+        /**
+         * Return HashSet of unique keys
+         * @return all unique keys found in both tables.
+         *
+         * Not in use.
+         */
         Set<String> getUniqueKeys() {
             return this.uniqueKeys;
         }
 
+        /**
+         * Message when an exception has been thrown
+         * @return error message
+         */
         String getMessage() {
             return this.message;
         }
 
-        void setMessage(String str) {
+        /**
+         * Set error message when an exception is thrown and caught
+         * @param str error message
+         */
+        private void setMessage(String str) {
             this.message = str;
         }
 
+        /**
+         * Flag to determine whether an error has occurred
+         * @return true or false
+         */
         boolean hasError() {
             return this.error;
         }
